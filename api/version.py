20fc7db0306930e4d463088f41099c208ec0a719
Jialifuniya <woshixubohan04@163.com>
2024-11-17 13:13:07 +0800

    update

---
+++ api/version.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=['get'])
+def main():
+    return jsonify(
+        {
+            'code': 200,
+            'version': '2.0.28',
+        }
+    )
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    app.json.ensure_ascii = False
